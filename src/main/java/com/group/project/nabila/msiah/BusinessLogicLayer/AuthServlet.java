9524a1ede7662858f10f0c3af350ccc8be4dc899
Nabila M. <msiahnabila@gmail.com>
2025-07-28 20:25:19 -0400

    Initial commit with base structure

---
+++ src/main/java/com/group/project/nabila/msiah/BusinessLogicLayer/AuthServlet.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.group.project.nabila.msiah.BusinessLogicLayer;
+
+/**
+ *
+ * @author SMILEY
+ */
+public class AuthServlet {
+    
+}
