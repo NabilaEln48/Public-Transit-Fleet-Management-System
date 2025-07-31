77d3b7ddd6a5d66d31f2f76948bb98d5864e1fb7
ZhiruXie <danielxie9735@gmail.com>
2025-07-30 20:47:08 -0400

    update my branch with new documentation, frontend controller and reportdashboard webpage along with its css

---
+++ src/main/java/ControllerLayer/FrontendController.java
@@ -0,0 +1,97 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ControllerLayer;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author 61963
+ */
+public class FrontendController extends HttpServlet{
+    /** The business logic instance that are used for calling detailed logic from the DAO implementation class. */
+//    private AuthorsBusinessLogic businessLogic = new AuthorsBusinessLogic();
+    /** Target url for new web page. */
+    private String targetUrl;
+    
+    /**Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+    * @param request servlet request
+    * @param response servlet response
+    */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String action = request.getParameter("action");
+        RequestDispatcher dispatcher;
+        
+        targetUrl = action;
+        
+        boolean prepareResult = false;
+        try{
+            prepareResult = prepareRequest(request, response, action);
+        }
+        catch(IOException e){
+        }
+        
+        if(!prepareResult){
+            return;
+        }
+        
+        dispatcher = request.getRequestDispatcher(targetUrl);       
+        dispatcher.forward(request, response);
+    }
+    
+    // <editor-fold defaultstate="collapsed" desc="Url process logic">
+    /** This method prepares the request based on the action type.
+    * @param request servlet request
+    * @param response servlet response
+    * @param action The action type to perform.
+    * @return Operation result. True if success and False if not.
+    * @throws NullPointerException if the database connection is null.
+    * @throws IOException if an I/O error occurs
+    */
+    private boolean prepareRequest(HttpServletRequest request, HttpServletResponse response, String action) throws IOException, NullPointerException{
+        switch (action){
+            case "ReportDashboard" -> {
+                
+            }
+        }
+        return true;
+    }
+    // </editor-fold>
+    
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+    // </editor-fold>
+}
--- src/main/java/com/group/project/nabila/msiah/DataAccessLayer/DataSource.java
+++
@@ -1,5 +0,0 @@
-package com.group.project.nabila.msiah.DataAccessLayer;
-
-public class DataSource {
-    
-}
